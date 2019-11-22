276a458a313736829401602d8a8b32b62f4b0cf1
Thomas Drevon <thomas@sanity.io>
2019-11-22 13:59:45 +0100

    Update schema

--- studio/schemas/shared.js
+++ studio/schemas/shared.js
@@ -1,10 +1,12 @@
 export default {
   systems: [
+    {title: 'Coriolis', value: 'coriolis'},
     {title: 'D&D 3.5e', value: 'd&d3.5e'},
     {title: 'D&D 4e', value: 'd&d4e'},
     {title: 'D&D 5e', value: 'd&d5e'},
     {title: 'Fate', value: 'fate'},
     {title: 'Pathfinder', value: 'pathfinder'},
+    {title: 'Pathfinder 2', value: 'pathfinder2'},
     {title: 'Shadow of the Demon Lord', value: 'sotdl'},
     {title: 'Warhammer', value: 'warhammer'},
     {title: 'freeform', value: 'freeform'}
