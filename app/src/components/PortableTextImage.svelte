ff7cd0740e5158937427674a8b82f35f8a378ae7
Thomas Drevon <thomas@drevon.no>
2024-04-19 09:43:44 +0200

    Click to see larger image

--- app/src/components/PortableTextImage.svelte
+++ app/src/components/PortableTextImage.svelte
@@ -3,7 +3,13 @@
 	export let portableText
 </script>
 
-<img src={urlFor(portableText.value, {isInline: true, width: 200}).url()} alt="" />
+<a
+	href={urlFor(portableText.value, {isInline: false, width: 800}).url()}
+	alt="Large image"
+	target="_blank"
+	rel="noopener noreferrer"
+	><img src={urlFor(portableText.value, {isInline: true, width: 200}).url()} alt="" /></a
+>
 
 <style>
 	img {
