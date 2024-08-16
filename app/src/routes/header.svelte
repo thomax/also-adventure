cb41d17838693e1a10e4fee7b96f990a5f937782
Thomas Drevon <thomas@drevon.no>
2024-08-16 11:42:29 +0200

    Fix a comment

--- app/src/routes/header.svelte
+++ app/src/routes/header.svelte
@@ -5,7 +5,7 @@
 <header class="header">
 	<a class="header__title" href="/">The Source of Adventure</a>
 	{#if window.location.pathname !== '/random/item'}
-		// A quick hack to hide the search input on the random item page
+		<!-- A quick hack to hide the search input on the random item page -->
 		<SearchInput />
 	{/if}
 </header>
