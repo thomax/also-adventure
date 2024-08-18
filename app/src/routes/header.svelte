6cb12ed5ae0761dde2280b1c5ea959195b8c7f1e
Thomas Drevon <thomas@drevon.no>
2024-08-18 10:11:54 +0200

    Fiddling wit the link to rnd item page

--- app/src/routes/header.svelte
+++ app/src/routes/header.svelte
@@ -4,23 +4,31 @@
 
 	let currentPath = browser ? window.location.pathname : undefined;
 
-	$: {
-		currentPath = browser ? window.location.pathname : undefined;
-	}
+	$: currentPath = browser ? window.location.pathname : undefined;
 
 </script>
 
 <header class="header">
-	<a class="header__title" href="/">The Source of <a href="/random/item" id="randomItemLink">Adventure</a></a>
+	<a class="header__title" href="/">The Source of Adventure</a>
 	{#if currentPath !== '/random/item'}
 		<!-- A quick hack to hide the search input on the random item page -->
 		<SearchInput />
 	{/if}
 </header>
+<a href="/random/item" id="randomItemLink">A random item</a>
 
 <style>
 	#randomItemLink {
+		position: absolute;
+		top: 10px;
+		right: 20%;
+		font-size: 0.5rem;
 		text-decoration: none;
-		color: rgb(16, 17, 18);
+		color: rgba(16, 17, 18, 0.8);
+		border-bottom: 1px solid transparent;
+	}
+
+	#randomItemLink:hover {
+		border-color: black;
 	}
 </style>
