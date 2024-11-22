1e70d0addb84553338caa8ab4f37562aca0da943
Thomas Drevon <thomas@drevon.no>
2024-11-22 11:37:07 +0100

    Properly render inline vs block images

--- app/src/components/PortableTextImage.svelte
+++ app/src/components/PortableTextImage.svelte
@@ -1,19 +1,48 @@
 <script>
 	import {urlFor} from '$lib/utils/image'
 	export let portableText
+	const shadowVariants = ['v1', 'v2', 'v3']
+	let ptImage = portableText.value
+	let isInline
+	let width
+	let shadowVariant
+	let classes
+
+	// Hack: since hotspot is only available on block images, use this to determine if the image is inline or block
+	$: {
+		ptImage = portableText.value
+		isInline = ptImage.hotspot ? false : true
+		width = isInline ? 200 : 735
+		// random shadow variant
+		shadowVariant = shadowVariants[Math.floor(Math.random() * shadowVariants.length)]
+		classes = [shadowVariant, isInline ? 'float-right' : 'block'].join(' ')
+	}
 </script>
 
-<a
-	href={urlFor(portableText.value).url()}
-	alt="Large image"
-	target="_blank"
-	rel="noopener noreferrer"
-	><img src={urlFor(portableText.value, {isInline: true, width: 200}).url()} alt="" /></a
+<a href={urlFor(ptImage).url()} alt="Large image" target="_blank" rel="noopener noreferrer"
+	><img src={urlFor(ptImage, {isInline, width}).url()} alt="" class={classes} /></a
 >
 
 <style>
-	img {
+	.v1 {
+		box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset,
+			rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+	}
+	.v2 {
+		box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+	}
+	.v3 {
+		box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+	}
+
+	.block {
+		display: block;
+		margin: 15px 0px;
+	}
+
+	.float-right {
 		float: right;
-		margin-right: 10px;
+
+		margin: 5px 0px 5px 10px;
 	}
 </style>
