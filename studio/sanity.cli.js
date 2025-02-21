764d84a4c6db944b4d53c97c9f91d64c6ff4c019
Thomas Drevon <thomas@drevon.no>
2025-02-21 09:21:39 +0100

    Add studiohost to config file

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -1,8 +1,9 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: "sajbthd8",
     dataset: "production"
-  }
+  },
+  studioHost: 'alsoadventure'
 })
